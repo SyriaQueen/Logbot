a0e17c4fe36ea0bd0f8f054555b5d279871ba6ec
jad <bawabhjad123@gmail.com>
2025-03-19 00:09:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  TOKEN: "MTM1MTEwNzU5MjczODQ0MzI3NQ.GZwNZX.EQaqLFyeg3hlY2ZXYQxiudn_rV7WUXMTCeJeBY", // ضع التوكن الخاص بالبوت هنا
+  TOKEN: "MTM1MTEwNzU5MjczODQ0MzI3NQ.G_P19t.sL8X4R6yQeiGkO2hsI8e6n3T8A0Z_di2SWyuzc", // ضع التوكن الخاص بالبوت هنا
   LOG_CHANNEL_ID: "1328375690109653096", // ضع معرف القناة التي سترسل إليها السجلات هنا
 };
