c9a306a11b867fb57b1598f6f28ab05716d85e86
jad <bawabhjad123@gmail.com>
2025-03-18 21:20:41 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  TOKEN: "MTM1MTEwNzU5MjczODQ0MzI3NQ.GZwNZX.EQaqLFyeg3hlY2ZXYQxiudn_rV7WUXMTCeJeBY", // ضع التوكن الخاص بالبوت هنا
+  LOG_CHANNEL_ID: "1328375690109653096", // ضع معرف القناة التي سترسل إليها السجلات هنا
+};
