c456fe7c167884c59a9a7991d536b351834a1e21
jad <bawabhjad123@gmail.com>
2025-03-22 11:45:37 +0300

    Update xo.js

--- commands/games/xo.js
+++ commands/games/xo.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
-const config = require('../config.js');
+const config = require('../../config.js');
 
 const activeGames = new Map();
 const gameMessages = new Map();
