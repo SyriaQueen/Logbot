ca3b8461b41e4a9371225e798621d8f33241b309
jad <bawabhjad123@gmail.com>
2025-04-05 09:17:10 +0300

    Update warn.js

--- commands/Mod/Warn/warn.js
+++ commands/Mod/Warn/warn.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder, PermissionsBitField } = require('discord.js');
-const config = require('../../config.js');
+const config = require('../../../config.js');
 
 module.exports = {
     name: 'warn',
